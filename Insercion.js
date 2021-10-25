e656c27ea29d63bffe3a461c4b198bebd9144ac0
GabyLizz <gabylizz.od@gmail.com>
2021-10-25 18:23:37 -0500

    2 files modified, 1 added

---
+++ Insercion.js
@@ -0,0 +1,28 @@
+const random = require('./random');
+
+const Insercion = (arr) => {
+  for (let i = 0; i <= 15; i++) {
+    let aleatorio = random(1, 5, 2);
+    //console.log(aleatorio);
+    numeros.push(aleatorio);
+  }
+
+  const l = numeros.length;
+  let j, temp;
+  //Mostramos los numeros desordenados
+  console.log(numeros);
+
+  for (let i = 1; i < l; i++) {
+    j = i;
+    temp = arr[i];
+    while (j > 0 && arr[j - 1] > temp) {
+      arr[j] = arr[j - 1];
+      j--;
+    }
+    arr[j] = temp;
+  }
+
+  console.log(arr);
+};
+
+module.export = Insercion;
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const Burbuja = require('./Burbuja');
+const Insercion = require('./Insercion');
 
+Console.log('Ordenamiento tipo Burbuja:');
 Burbuja();
+
+Console.log('Ordenamiento tipo Insercion:');
+Insercion();
